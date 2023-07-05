49e4948c98069c93bfcc85c1a373d3c372a14fb4
Monique Lima Ramos <moniquelima19@hotmail.com>
2023-07-05 19:40:08 -0300

    JavaSpript part4

--- sripts.js
+++ sripts.js
@@ -33,10 +33,27 @@ function convertValues() {
         currency: "BRL"
     }).format(inputCurrencyValue)
 
-
-
-
-    console.log(convertedValue)
 }
 
+function changeCurrency() {
+    const currencyName = document.getElementById('currency-name')
+    const currencyImage = document.querySelector('.currency-img')
+
+
+    if (currencySelect.value == 'dolar') {
+        currencyName.innerHTML = 'Dolar americano'
+        currencyImage.src = './assets/dolar.png'
+    }
+
+    if (currencySelect.value == 'euro') {
+        currencyName.innerHTML = 'Euro'
+        currencyImage.src ='./assets/euro.png'
+    
+}
+
+convertValues()
+
+}
+
+currencySelect.addEventListener('change', changeCurrency)
 convertButton.addEventListener("click", convertValues)
